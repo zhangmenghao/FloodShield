094f30782edfee0a8774afd1db66f90f7db566c5
baijiasong <bjs13cst@gmail.com>
2017-06-28 22:53:08 +0800

    fix bug

--- src/main/java/net/floodlightcontroller/statistics/ShieldManager.java
+++ src/main/java/net/floodlightcontroller/statistics/ShieldManager.java
@@ -4,13 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-import org.projectfloodlight.openflow.types.DatapathId;
 import org.projectfloodlight.openflow.types.IPv4Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
-
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
